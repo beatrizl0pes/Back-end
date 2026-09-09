a4c990417ca8b8f86e048af5f25e6428ac54d651
beatrizl0pes <beatriz.v.lopes6@aluno.senai.br>
2026-09-09 10:37:09 -0300

    Aula de PHP

---
+++ lista9/ex6.php
@@ -0,0 +1,12 @@
+<?php
+
+$musica = ["Peão todo tatuado", "Happy nation", "Happy pills", "Single Ladies"];
+
+echo "Playlist da Bia<br><br>";
+
+foreach($musica as $listas){
+    echo $listas, "<br>";
+}
+echo "<br> Quantidade de músicas: ", count($musica);
+
+?>
